f18c2ef96683db7f6b646b61670d2a4d8f75b51b
mfrobinson <mrobinson385a@gmail.com>
2026-05-05 12:20:37 +0100

    Updated BaseClientInfo.h

--- Include/Proto/Net/IP/TCP/Server/MultithreadedServer/Info/BaseClientInfo.h
+++ Include/Proto/Net/IP/TCP/Server/MultithreadedServer/Info/BaseClientInfo.h
@@ -1,10 +1,16 @@
 #pragma once
 #include "Proto/Net/IP/TCP/DataSocket.h"
 #include <mutex>
+#include <list>
+#include <memory>
 
 
 namespace proto::net::ip::tcp {
 
+	template <typename TExtraClientInfo>
+	class ClientInfo;
+
+	template <typename TExtraClientInfo>
 	struct BaseClientInfo {
 		DataSocket socket;
 		typename std::list<std::weak_ptr<ClientInfo<TExtraClientInfo>>>::iterator registry_position;
